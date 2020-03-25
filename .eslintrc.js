2fab0a10db678974fc9ffd68f60f3481a08cda4c
Marina Mosti <marina@mosti.com.mx>
2020-03-25 15:32:51 -0600

    Remove vue single root eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/valid-template-root': 'off'
   }
 }
