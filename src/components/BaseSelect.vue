1c602d77c3895ce8ff7d9877654a678cf73d194c
Marina Mosti <marina@mosti.com.mx>
2020-03-25 15:34:49 -0600

    Adds BaseSelect component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,37 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <select :value="modelValue" @change="updateValue" v-bind="$attrs">
+    <option
+      v-for="option in options"
+      :value="option"
+      :key="option"
+      :selected="option === modelValue"
+    >{{ option }}</option>
+  </select>
+</template>
+<script>
+import SetupFormInput from '@/features/SetupFormInput'
+
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number]
+    }
+  },
+  setup (props, context) {
+    const { updateValue } = SetupFormInput(props, context)
+
+    return {
+      updateValue
+    }
+  }
+}
+</script>
