8666990e01b3a34fcf7f92144933ed504fc75521
Marina Mosti <marina@mosti.com.mx>
2020-03-25 15:35:10 -0600

    Adds Simple and Component form views

---
+++ src/views/ComponentsForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <h1>Create an event</h1>
+
+    <BaseSelect
+      label="Select a category"
+      :options="categories"
+      v-model="event.category"
+    />
+
+    {{ event }}
+  </form>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+const categories = [
+  'sustainability',
+  'nature',
+  'animal welfare',
+  'housing',
+  'education',
+  'food',
+  'community'
+]
+
+export default {
+  setup () {
+    const submitForm = () => { console.log('Submit') }
+    const event = reactive({
+      category: ''
+    })
+
+    return {
+      submitForm,
+      event,
+      categories
+    }
+  }
+}
+</script>
---
+++ src/views/SimpleForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <h1>Create an event</h1>
+
+    <label>
+      Select a category
+      <select name="category" v-model="event.category">
+        <option v-for="category in categories" :value="category" :key="category">{{ category }}</option>
+      </select>
+    </label>
+
+    {{ event }}
+  </form>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+const categories = [
+  'sustainability',
+  'nature',
+  'animal welfare',
+  'housing',
+  'education',
+  'food',
+  'community'
+]
+
+export default {
+  setup () {
+    const submitForm = () => { console.log('Submit') }
+    const event = reactive({
+      category: ''
+    })
+
+    return {
+      submitForm,
+      event,
+      categories
+    }
+  }
+}
+</script>
