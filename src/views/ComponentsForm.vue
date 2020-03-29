07e76a117f1695ec77ca15c6a6f499b416be3110
Marina Mosti <marina@mosti.com.mx>
2020-03-28 19:39:13 -0600

    Update Simple and Component based form examples

--- src/views/ComponentsForm.vue
+++ src/views/ComponentsForm.vue
@@ -1,15 +1,44 @@
 <template>
-  <form @submit.prevent="submitForm">
-    <h1>Create an event</h1>
+  <div>
+    <h1>Create an Event</h1>
+    <form @submit.prevent="createEvent">
+      <BaseSelect
+        label="Select a category"
+        :options="categories"
+        v-model="event.category"
+      />
 
-    <BaseSelect
-      label="Select a category"
-      :options="categories"
-      v-model="event.category"
-    />
+      <h3>Name & describe your event</h3>
+      <BaseInput
+        label="Title"
+        v-model="event.title"
+        type="text"
+        placeholder="Title"
+        class="field"
+      />
 
-    {{ event }}
-  </form>
+      <BaseInput
+        label="Description"
+        v-model="event.description"
+        type="text"
+        placeholder="Description"
+        class="field"
+      />
+
+      <h3>Where is your event?</h3>
+      <BaseInput
+        label="Location"
+        v-model="event.location"
+        type="text"
+        placeholder="Location"
+        class="field"
+      />
+
+      <BaseButton type="submit" class="-fill-gradient">Submit</BaseButton>
+    </form>
+
+    <pre>{{ event }}</pre>
+  </div>
 </template>
 
 <script>
@@ -29,7 +58,10 @@ export default {
   setup () {
     const submitForm = () => { console.log('Submit') }
     const event = reactive({
-      category: ''
+      category: '',
+      title: '',
+      description: '',
+      location: ''
     })
 
     return {
--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -1,16 +1,51 @@
 <template>
-  <form @submit.prevent="submitForm">
+  <div>
     <h1>Create an event</h1>
+    <form @submit.prevent="submitForm">
 
-    <label>
-      Select a category
-      <select name="category" v-model="event.category">
-        <option v-for="category in categories" :value="category" :key="category">{{ category }}</option>
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option
+          v-for="option in categories"
+          :value="option"
+          :key="option"
+          :selected="option === event.category"
+        >{{ option }}</option>
       </select>
-    </label>
 
-    {{ event }}
-  </form>
+      <h3>Name & describe your event</h3>
+
+      <label>Title</label>
+      <input
+        v-model="event.title"
+        type="text"
+        placeholder="Title"
+        class="field"
+      >
+
+      <label>Description</label>
+      <input
+        v-model="event.description"
+        type="text"
+        placeholder="Description"
+        class="field"
+      />
+
+      <h3>Where is your event?</h3>
+
+      <label>Location</label>
+      <input
+        v-model="event.location"
+        type="text"
+        placeholder="Location"
+        class="field"
+      />
+
+      <button type="submit">Submit</button>
+    </form>
+
+    <pre>{{ event }}</pre>
+  </div>
 </template>
 
 <script>
@@ -30,7 +65,10 @@ export default {
   setup () {
     const submitForm = () => { console.log('Submit') }
     const event = reactive({
-      category: ''
+      category: '',
+      title: '',
+      description: '',
+      location: ''
     })
 
     return {
