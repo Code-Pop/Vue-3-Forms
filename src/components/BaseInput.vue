0fabd7ba7c11fd98e00d3aa9adc084135a80f091
Marina Mosti <marina@mosti.com.mx>
2020-03-28 19:38:42 -0600

    Add BaseButton and Input

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <input :value="modelValue"
+      v-bind="{
+        ...$attrs,
+        onInput: updateValue
+      }"
+    >
+  </div>
+</template>
+
+<script>
+import SetupFormInput from '@/features/SetupFormInput'
+
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number]
+    }
+  },
+  setup (props, context) {
+    const { updateValue } = SetupFormInput(props, context)
+
+    return {
+      updateValue
+    }
+  }
+}
+</script>
