5fddf8cfcd607e5c162707cefb0506aaaa4699ff
Marina Mosti <marina@mosti.com.mx>
2020-07-25 20:03:50 +0100

    add radio button group

---
+++ src/components/BaseRadioGroup.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <component
+      v-for="opts in options"
+      :key="opts.value"
+      :is="vertical ? 'div' : 'span'"
+      :style="vertical ? '' : 'margin-right: 20px;'"
+    >
+      <BaseRadio
+        :name="name"
+        :label="opts.label"
+        :modelValue="modelValue"
+        :value="opts.value"
+        @update:modelValue="$emit('update:modelValue', $event)"
+      />
+    </component>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    vertical: {
+      type: Boolean,
+      default: false
+    },
+    modelValue: {
+      type: [String, Number],
+      required: true
+    },
+    options: {
+      type: Array,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
