843c4793b5bfa3f78027818645cbab323f5d8040
Marina Mosti <marina@mosti.com.mx>
2020-08-24 09:21:49 +0200

    update form

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -2,16 +2,11 @@
   <div>
     <h1>Create an event</h1>
     <form>
-
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option
-          v-for="option in categories"
-          :value="option"
-          :key="option"
-          :selected="option === event.category"
-        >{{ option }}</option>
-      </select>
+      <BaseSelect
+        :options="categories"
+        v-model="event.category"
+        label="Select a category"
+      />
 
       <h3>Name & describe your event</h3>
 
