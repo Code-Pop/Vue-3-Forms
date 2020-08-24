f1c98f4caf1b702bf69b8fc81c8eb0c592823f8b
Marina Mosti <marina@mosti.com.mx>
2020-08-24 09:21:43 +0200

    create base select

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,37 @@
+<template>
+  <label v-if="label">{{ label }}</label>
+  <select
+    class="field"
+    :value="modelValue"
+    v-bind="{
+      ...$attrs,
+      onChange: ($event) => { $emit('update:modelValue', $event.target.value) }
+    }"
+  >
+    <option
+      v-for="option in options"
+      :value="option"
+      :key="option"
+      :selected="option === modelValue"
+    >{{ option }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    },
+    options: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
