520e3104b6baa607513da2a5e9a3d9089e974ab3
Marina Mosti <marina@mosti.com.mx>
2021-02-19 18:16:26 +0100

    fix: trade false for null to remove attr

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -8,7 +8,7 @@
     class="field"
     :id="uuid"
     :aria-describedby="error ? `${uuid}-error` : null"
-    :aria-invalid="error ? true : false"
+    :aria-invalid="error ? true : null"
   >
   <p
     v-if="error"
